ab2962ff5e7b12f68483a757135df6dc90228da7
TVEGesdMC <783322+githubmain@students.mcallenisd.net>
2025-08-17 00:13:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ let clickValue = 1;
 let miningDrones = 0;
 let advancedDrones = 0;
 let spaceStations = 0;
+let lastSaveTime = Date.now();
+let achievements = [];
 
 const astroPointsElement = document.getElementById('astroPoints');
+const achievementMessage = document.getElementById('achievementMessage');
 
 function clickForPoints() {
     astroPoints += clickValue;
@@ -45,11 +48,46 @@ function buySpaceStation() {
 
 function updateDisplay() {
     astroPointsElement.textContent = astroPoints;
+    saveGame();
+}
+
+function saveGame() {
+    localStorage.setItem('astroPoints', astroPoints);
+    localStorage.setItem('clickValue', clickValue);
+    localStorage.setItem('miningDrones', miningDrones);
+    localStorage.setItem('advancedDrones', advancedDrones);
+    localStorage.setItem('spaceStations', spaceStations);
+    localStorage.setItem('lastSaveTime', Date.now());
+}
+
+function loadGame() {
+    if (localStorage.getItem('astroPoints')) {
+        astroPoints = parseInt(localStorage.getItem('astroPoints'));
+        clickValue = parseInt(localStorage.getItem('clickValue'));
+        miningDrones = parseInt(localStorage.getItem('miningDrones'));
+        advancedDrones = parseInt(localStorage.getItem('advancedDrones'));
+        spaceStations = parseInt(localStorage.getItem('spaceStations'));
+        lastSaveTime = parseInt(localStorage.getItem('lastSaveTime'));
+
+        const timeElapsed = (Date.now() - lastSaveTime) / 1000;
+        astroPoints += timeElapsed * (miningDrones * 1 + advancedDrones * 10 + spaceStations * 100);
+    } else {
+        achievements.push('Welcome to Astroclicker!');
+        displayAchievement();
+    }
+    updateDisplay();
+}
+
+function displayAchievement() {
+    achievementMessage.textContent = achievements.join('\n');
+    setTimeout(() => {
+        achievementMessage.textContent = '';
+    }, 5000);
 }
 
 setInterval(() => {
-    astroPoints += miningDrones * 1;
-    astroPoints += advancedDrones * 10;
-    astroPoints += spaceStations * 100;
+    astroPoints += miningDrones * 1 + advancedDrones * 10 + spaceStations * 100;
     updateDisplay();
 }, 1000);
+
+loadGame();
